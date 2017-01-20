2314f2c4606fcf0b8f1aeafa6b2a594faefcd303
Mariano Molina <xsin.crueldadx@gmail.com>
2017-01-19 23:01:21 -0300

    add material icons

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -8,8 +8,18 @@ class Appbar extends Component {
 
     render() {
         return (
-            <header className="Appbar">
-                <img src={brandIcon} alt="almundo.com" />
+            <header className="appbar">
+                <a href="#" className="appbar_icon" title="almundo.com">
+                    <img src={brandIcon} alt="almundo.com" />
+                </a>
+
+
+
+                <i className="material-icons md-light">person</i>
+
+                <i className="material-icons md-light">phone</i>
+
+
             </header>
         );
     }
